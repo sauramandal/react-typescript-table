8b212dc3c4f4312fa0ff1dcb178b52c7915a3380
Saura Mandal <sauramandal@Sauras-MacBook-Air.local>
2021-07-06 11:57:29 +0530

    Add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
 import { useState, useEffect } from "react";
 import personData from "./data/person-data.json";
 
+type Person = any;
+type Location = {
+  location_city: { header: string; value: string };
+  location_coordinates_latitude: { header: string; value: string };
+  location_coordinates_longitude: { header: string; value: string };
+  location_country: { header: string; value: string };
+  location_postcode: { header: string; value: number };
+  location_state: { header: string; value: string };
+  location_street_name: { header: string; value: string };
+  location_street_number: { header: string; value: number };
+  location_timezone_description: {
+    header: string;
+    value: string;
+  };
+  location_timezone_offset: { header: string; value: string };
+};
 const clone = (obj: any) => JSON.parse(JSON.stringify(obj));
 const flattenDeepObject = (
   location: any,
@@ -29,7 +45,7 @@ const transformFlattenLocations = (locations: any[]): any[] => {
 };
 
 const App = () => {
-  const [flattnedLocations, setFlattnedLocations] = useState<any []>([]);
+  const [flattnedLocations, setFlattnedLocations] = useState<any[]>([]);
   useEffect(() => {
     const draftFlattenedLocations: any[] = transformFlattenLocations(
       personData.results.map(({ location }) => location)
@@ -42,17 +58,24 @@ const App = () => {
       <table className="table">
         <thead>
           <tr>
-            {flattnedLocations[0] && Object.keys(flattnedLocations[0]).map((location, idx) => (
-              <th key={idx}>{(flattnedLocations[0][location]['header'])}</th>
-            ))}
+            {flattnedLocations[0] &&
+              Object.keys(flattnedLocations[0] as Location[]).map(
+                (location: string, idx: number) => (
+                  <th key={idx}>{flattnedLocations[0][location]["header"]}</th>
+                )
+              )}
           </tr>
         </thead>
         <tbody>
-          {flattnedLocations && flattnedLocations.map((item, itemIndex) => (
-            <tr key={itemIndex}>
-              {item && Object.keys(item).map((key, keyIdx) => <td key={keyIdx}>{item[key]['value']}</td>)}
-            </tr>
-          ))}
+          {flattnedLocations &&
+            flattnedLocations.map((item: any, itemIndex: number) => (
+              <tr key={itemIndex}>
+                {item &&
+                  Object.keys(item).map((key: string, keyIdx: number) => (
+                    <td key={keyIdx}>{item[key]["value"]}</td>
+                  ))}
+              </tr>
+            ))}
         </tbody>
       </table>
       {/* <pre>{JSON.stringify(flattnedLocations)}</pre> */}
